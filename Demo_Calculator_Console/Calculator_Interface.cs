be00478d418b4183a0487940f6f0dbf05af20dbd
amitbhola03 <amitbhola03@gmail.com>
2015-05-14 22:36:43 -0500

    test

--- Demo_Calculator_Console/Calculator_Interface.cs
+++ Demo_Calculator_Console/Calculator_Interface.cs
@@ -12,36 +12,28 @@ namespace Demo_Calculator_Console
         public Int64 func_Add(Int64 variable1, Int64 variable2 );
         public Int32 func_Add(Int32 variable1, Int32 variable2);
         //Can Interface have a Defination ?? 
-        public Int16 func_Add(Int16 variable1, Int16 variable2)
-        {
-            return (Int16)(variable1 + variable2);
-        }
+        public Int16 func_Add(Int16 variable1, Int16 variable2);
+
 
         //Declarations for the Subtract functions
         public Int64 func_Sub(Int64 variable1, Int64 variable2);
         public Int32 func_Sub(Int32 variable1, Int32 variable2);
         //Can Interface have a Defination ?? 
-        public Int16 func_sub(Int16 variable1, Int16 variable2)
-        {
-            return (Int16)(variable1 - variable2); 
-        }
+        public Int16 func_sub(Int16 variable1, Int16 variable2);
+
 
         //Declarations for the Multiply function
         public Int64 func_Mul(Int64 variable1, Int64 variable2);
         public Int32 func_Mul(Int32 variable1, Int32 variable2);
         //Can Interface have a Defination ?? 
-        public Int16 func_Mul(Int16 variable1, Int16 variable2)
-        {
-            return (Int16)(variable1 * variable2);
-        }
+        public Int16 func_Mul(Int16 variable1, Int16 variable2);
+
 
         //Declarations for the Divide function
         public Int64 func_Div(Int64 variable1, Int64 variable2);
         public Int32 func_Div(Int32 variable1, Int32 variable2);
         //Can Interface have a Defination ?? 
-        public Int16 func_Div(Int16 variable1, Int16 variable2)
-        {
-            return (Int16)(variable1 / variable2);
-        }
+        public Int16 func_Div(Int16 variable1, Int16 variable2);
+
     }
 }

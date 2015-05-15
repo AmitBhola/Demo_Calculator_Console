6298e70ec00455d2a0b9f4d1568004c150199845
amitbhola03 <amitbhola03@gmail.com>
2015-05-14 22:21:56 -0500

    Adding Calculator.cs file

---
+++ Demo_Calculator_Console/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Calculator_Console
+{
+    class Calculator 
+    {
+        private static Calculator_Functions cf = new Calculator_Functions();
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Start of the Calculator\n\n");
+                Console.WriteLine("Write computaion in the following format Operand_1<Operation>Operand_2\n");
+                //Obtain the user input from the screen
+                string input = Console.ReadLine();
+
+                if (input.Contains('+'))
+                {
+                    // Call Appropriate function to handle Input
+                    cf.handleAddition(input);
+                }
+                else if (input.Contains('-'))
+                {
+                    cf.handleSubtraction(input);
+                }
+                else if (input.Contains('*'))
+                {
+                    cf.handleMultiplication(input);
+                }
+                else if (input.Contains('/'))
+                {
+                    cf.handleDivision(input);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Format\n ");
+                }
+                //Pause the output
+                Console.ReadLine();
+                //Break the While loop
+                Console.WriteLine("Press Q/q to Quit | Press any Key to Continue");
+                string exit = Console.ReadLine();
+                if (exit.Equals("q") || exit.Equals("Q"))
+                {
+                    break;
+                }
+                        
+            }
+        }
+
+    }
+}
